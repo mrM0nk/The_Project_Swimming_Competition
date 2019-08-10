37fdedd37c58e4366470fa8c9a8c0ea521fd289a
sergey <s.k1990@mail.ru>
2019-08-10 15:38:03 +0300

    вариант Сергея

---
+++ competition.py
@@ -0,0 +1,70 @@
+import json
+
+
+class result:
+
+    def __init__(self, fistname, lastname, year_of_birth, gender, club=None, city=None, time=None, disqualification=None):
+        self.fistname = fistname
+        self.lastname = lastname,
+        self.year_of_birth = int(year_of_birth)
+        self.gender = gender
+        self.club = {"club": club, "city": city}
+        self.time = time
+        self.disqualification = disqualification
+
+    def get_attributes(self):
+        keys = ["fistname", "lastname", "year_of_birth", "gender", "club", "time", "disqualification"]
+        values = [self.fistname,
+                  self.lastname,
+                  self.year_of_birth,
+                  self.gender,
+                  self.club,
+                  self.time,
+                  self.disqualification]
+        return dict.fromkeys(keys, values)
+
+
+class event:
+
+    def __init__(self, title, location, date):
+        self.title = title
+        self.location = location
+        self.date = date
+
+    def get_attributes(self):
+        keys = ["title", "location", "date"]
+        values = [self.title, self.location, self.date]
+        return dict.fromkeys(keys, values)
+
+class pool:
+
+    def __init__(self, title, size):
+        self.title = title
+        self.size = size
+
+    def get_attributes(self):
+        return {"title": self.title, "size": self.size}
+
+
+class competition:
+
+    def __init__(self, event, pool, category, style, distance, results=None):
+        self.event = event
+        self.pool = pool
+        self.category = category
+        self.discipline = {"style": style, "distance": distance}
+        self.results = [] if results is None else results
+
+    def add_result(self):
+        pass
+
+    def get_attributes(self):
+        results = [ res.get_attributes() for res in self.results]
+        keys = ["event", "pool", "category", "discipline", "results"]
+        values = [self.event.get_attributes(),
+                  self.pool.get_attributes(),
+                  self.category,
+                  self.discipline,
+                  results]
+        return dict.fromkeys(keys, values)
+
